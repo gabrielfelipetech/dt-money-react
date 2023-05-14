6972273173cc1971fc525b51f773fe0cae0732b5
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2023-05-14 13:45:35 -0300

    feat: finish app

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,5 +1,6 @@
-import { ReactNode, createContext, useEffect, useState } from 'react'
+import { ReactNode,  useEffect, useState, useCallback } from 'react'
 import { api } from '../lib/axios'
+import { createContext } from 'use-context-selector'
 
 interface Transaction {
   id: number
@@ -26,7 +27,7 @@ interface TransactionProviderProps {
 export const TransactionsContext = createContext({} as TransactionContextType)
 export function TransactionsProvider({ children }: TransactionProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get('transactions', {
       params: {
         _sort: 'createdAt',
@@ -35,8 +36,8 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
       },
     })
     setTransactions(response.data)
-  }
-  async function createTransaction(data: CreateTransactionInput) {
+  }, [],)
+  const createTransaction = useCallback(async (data: CreateTransactionInput) => {
     const { description, price, category, type } = data
     const response = await api.post('transactions', {
       description,
@@ -46,7 +47,7 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
       createdAt: new Date(),
     })
     setTransactions((state) => [response.data, ...state])
-  }
+  }, [],)
   useEffect(() => {
     fetchTransactions()
   }, [])
